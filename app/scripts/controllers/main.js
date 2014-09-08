441f2800777b1ec125dd4b702526055a0b01c7a9
Andrew Keym <andrew.keym@gmail.com>
2014-09-07 21:15:37 -0400

    Cleaned up look and feel of translating...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,15 @@ angular.module('kraangApp')
   .controller('MainCtrl', function ($scope, translate) {
 
     $scope.talkKraang = function (sentence) {
+      $scope.translating = true;
       translate.createSentence({ 'input_text': sentence })
       .then(function (response) {
         $scope.results = response.data;
+        $scope.translating = false;
       });
+    };
+
+    $scope.reset = function () {
+      $scope.results = null;
     };
   });
