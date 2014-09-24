9e6c58133001bb48d252bd484e1ed0fb96e62520
Andrew Keym <andrew.keym@gmail.com>
2014-09-24 07:21:54 -0400

    Removed mespeak library - added SpeechSynthesis

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Kraang UI
 =========
 
 This is the front-end for kraang.io. It's an Angular project using the [Yeoman Angular generator](https://github.com/yeoman/generator-angular). All of the `grunt` tasks apply to this project.
+
+[ ![Codeship Status for keymholio/kraang-ui](https://codeship.io/projects/7fd40fb0-260a-0132-ce11-5220aac52c67/status)](https://codeship.io/projects/37322)
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,28 +8,46 @@
  * Controller of the kraangApp
  */
 
+ /* global speechSynthesis, SpeechSynthesisUtterance */
+
 angular.module('kraangApp')
   .controller('MainCtrl', function ($scope, translate) {
 
-    $scope.talkKraang = function (sentence) {
+    $scope.consecutive = false;
+
+    $scope.talkKraang = function (sentence, consecutive) {
+
       if (sentence) {
         $scope.translating = true;
         translate.createSentence({ 'input_text': sentence })
         .then(function (response) {
           $scope.results = response.data;
 
-          /* jshint ignore:start */
-          var speakSentence = response.data.kraang.replace(/kraang/gi, 'krang');
-          meSpeak.loadConfig("/json/mespeak_config.json");
-          meSpeak.loadVoice('/json/en-us.json');
-          meSpeak.speak(speakSentence, { variant: 'm2', pitch: 30 });
-          /* jshint ignore:end */
+            window.speechSynthesis.onvoiceschanged = function() {
+              $scope.speak(response.data.kraang);
+            };
+
+            if (consecutive) {
+              $scope.speak(response.data.kraang);
+            }
+
           $scope.translating = false;
         });
       }
     };
 
-    $scope.reset = function () {
+    $scope.speak = function (sentence) {
+      if ('speechSynthesis' in window) {
+        var u = new SpeechSynthesisUtterance();
+        var voices = window.speechSynthesis.getVoices();
+        u.voice = voices[33]; // Zarvox
+        u.text = sentence.replace(/kraang/gi, 'krang');
+        speechSynthesis.speak(u);
+      }
+    };
+
+    $scope.again = function () {
       $scope.results = null;
+      $scope.consecutive = true;
     };
   });
